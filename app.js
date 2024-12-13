709b5efedc06766402db564acb3b3fef71b743f9
Billy Mitchell <hello@billymitchell.design>
2024-12-13 13:03:06 -0500

    update allow origins

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set up CORS to allow requests from both www and non-www versions of the domain
-const allowedOriginsRegex = /^https?:\/\/(www\.)?ftg-redemption-test\.mybrightsites\.com$/;
+// Secure headers
+app.use(helmet());
+
+// Set up CORS
+const allowedOrigins = [
+  /^https?:\/\/(www\.)?ftg-redemption-test\.mybrightsites\.com$/,
+  /^https?:\/\/(www\.)?ftg-redemption\.mybrightsites\.com$/,
+  /^https?:\/\/(www\.)?redeem\.forbestravelguide\.com$/
+];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Check if the origin matches the regex or is undefined (for same-origin requests)
-    if (allowedOriginsRegex.test(origin) || !origin) {
+    if (allowedOrigins.some(regex => regex.test(origin)) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  // credentials: true, // Enable if cookies are needed for cross-origin requests
 }));
 
 // Middleware to parse JSON body
 app.use(express.json());
 
-// Set up rate limiting for incoming requests
+// Rate Limiting
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 100, // Limit each IP to 100 requests per minute
@@ -36,6 +42,15 @@ const receiveOrderData = require('./routes/receiveOrderData');
 // Use the routes
 app.use('/api/redemption-code-status', getRedemptionStatus);
 app.use('/api', receiveOrderData);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).json({ error: err.message });
+  } else {
+    next();
+  }
+});
 
 // Start the server
 app.listen(PORT, () => {
